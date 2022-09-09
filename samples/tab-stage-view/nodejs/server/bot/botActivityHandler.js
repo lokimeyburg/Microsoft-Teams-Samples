a39b6d6628d5bfb81396a360e76e89abd3d1873d
Loki Meyburg <loki@lokimeyburg.com>
2022-09-08 20:35:50 -0700

    Stage View with link unfurling support

--- samples/tab-stage-view/nodejs/server/bot/botActivityHandler.js
+++ samples/tab-stage-view/nodejs/server/bot/botActivityHandler.js
@@ -21,6 +21,60 @@ class BotActivityHandler extends TeamsActivityHandler  {
           await next();
       });
     }
+
+    handleTeamsAppBasedLinkQuery(context, query) {
+        const attachment = CardFactory.adaptiveCard(adaptiveCards.adaptiveCardForTabStageView(query.url));
+        attachment.preview = {
+            content: {
+              title: "Description",
+              text: "Title",
+              images: [
+                {
+                  url: process.env.BaseUrl + "logo.svg",
+                },
+              ],
+            },
+            contentType: "application/vnd.microsoft.card.thumbnail",
+        }
+
+        const result = {
+          attachmentLayout: 'list',
+          type: 'result',
+          attachments: [attachment],
+          responseType: "composeExtension"
+        };
+    
+        const response = {
+          composeExtension: result
+        };
+
+        return response;
+    }
+
+    async handleTeamsMessagingExtensionQuery(context, query) {
+        // Note: The Teams manifest.json for this sample also inclues a Search Query, in order to enable installing from App Studio.
+        // const searchQuery = query.parameters[0].value;
+        const heroCard = CardFactory.heroCard('This is a Link Unfurling Sample',
+            'This sample demonstrates how to handle link unfurling in Teams.  Please review the readme for more information.');
+        heroCard.content.subtitle = 'It will unfurl links from *.BotFramework.com';
+        const attachment = { ...heroCard, heroCard };
+
+        switch (query.commandId) {
+        case 'searchQuery':
+            return {
+                composeExtension: {
+                    type: 'result',
+                    attachmentLayout: 'list',
+                    attachments: [
+                        attachment
+                    ]
+                }
+            };
+        default:
+            throw new Error('NotImplemented');
+        }
+    }
+
 }
 
 module.exports.BotActivityHandler = BotActivityHandler;
